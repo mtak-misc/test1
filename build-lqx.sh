ce06469d93f28b67568575def544e094c6fbb8ff
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2022-12-23 23:10:36 +0900

    Update build-lqx.sh

--- build-lqx.sh
+++ build-lqx.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 pacman -Syu --noconfirm base-devel sudo git jq curl unzip schedtool
-#curl -sLJO -H 'Accept: application/octet-stream' \
-#"https://$GITHUB_TOKEN@api.github.com/repos/mtak-misc/pkgbuild-llvm-git/releases/assets/$( \
-#curl -sL https://$GITHUB_TOKEN@api.github.com/repos/mtak-misc/pkgbuild-llvm-git/releases/tags/test \
-#| jq '.assets[] | select(.name | contains("llvm")) | .id')" -o llvm-git.zip
-#unzip llvm-git.zip
-#pacman --noconfirm -U llvm-git*.pkg.tar.zst llvm-libs-git*.pkg.tar.zst
+curl -sLJO -H 'Accept: application/octet-stream' \
+"https://$GITHUB_TOKEN@api.github.com/repos/mtak-misc/pkgbuild-llvm-git/releases/assets/$( \
+curl -sL https://$GITHUB_TOKEN@api.github.com/repos/mtak-misc/pkgbuild-llvm-git/releases/tags/test \
+| jq '.assets[] | select(.name | contains("llvm")) | .id')" -o llvm-git.zip
+unzip llvm-git.zip
+pacman --noconfirm -U llvm-git*.pkg.tar.zst llvm-libs-git*.pkg.tar.zst
 useradd builder  -u $USERID -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 su builder -c "gpg --recv 38DBBDC86092693E"
 cd /workspace/linux-lqx ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" makepkg --noconfirm -sc"
