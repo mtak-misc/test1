e09a45a985d80e4e63fd0849a759105441e65486
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2023-10-29 10:10:47 +0900

    Update chat.py

--- python/chat.py
+++ python/chat.py
@@ -5,8 +5,8 @@ import copy
 import time
 from llama_cpp import Llama
 
-model_name_or_path = "mmnga/japanese-stablelm-instruct-gamma-7b-gguf"
-model_basename = "japanese-stablelm-instruct-gamma-7b-q8_0.gguf" 
+model_name_or_path = "TheBloke/Mistral-7B-Instruct-v0.1-GGUF"
+model_basename = "mistral-7b-instruct-v0.1.Q8_0.gguf" 
 
 if len(sys.argv) > 2:
     model_name_or_path = sys.argv[1]
