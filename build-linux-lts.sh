b58e7f3ca691963ab886f2ce8cc7e448ba756223
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-04-20 13:49:52 +0900

    Create build-linux-lts.sh

---
+++ build-linux-lts.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+USERID=$1
+
+apk upgrade
+apk add alpine-sdk sudo
+
+adduser builder -u ${USERID} -D -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+addgroup builder abuild
+chown builder -R ..
+chown builder -R /tmp
+mkdir -p /var/cache/distfiles
+chmod a+w /var/cache/distfiles
+chgrp abuild /var/cache/distfiles
+chmod g+w /var/cache/distfiles
+su builder -c "abuild-keygen -a -i -n"
+apk add bash git
+
+cd linux-lts
+sed -i -e '/unset LDFLAGS/a \ \tscripts/config --file "$srcdir"/$_config -e CONFIG_MOUSE_PS2_SENTELIC' APKBUILD
+
+su builder -c "abuild checksum"
+su builder -c "abuild deps"                
+
+su builder -c "abuild"        
+cp $(find /home/builder -name linux-lts*.apk) .
