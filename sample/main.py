2b0b04fcea071906ebf8e33f742b679750d4e83d
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-06-04 07:48:36 +0900

    Update main.py

--- sample/main.py
+++ sample/main.py
@@ -1,8 +1,27 @@
-from kivy.app import App
-from kivy.uix.button import Button
- 
-class TestApp(App):
+from kivy.uix.widget import Widget
+from kivymd.app import MDApp
+from webview import WebView
+from kivy.lang.builder import Builder
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.screen import MDScreen
+
+Builder.load_string("""
+<MyWebView>
+    MDFlatButton:
+        text: "Push"
+        pos_hint: {"center_x": .5, "center_y": .4}
+        on_press: root.Push()
+""")
+
+class MyWebView(MDScreen):
+    def Push(self):
+        WebView("https://www.google.com")
+
+
+class MyWebApp(MDApp):
     def build(self):
-        return Button(text='Hello World')
- 
-TestApp().run()
+        return MyWebView()
+
+
+if __name__ == '__main__':
+    MyWebApp().run()
