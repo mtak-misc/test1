5b9cc6afa73eb03104febf4bb8024ffa7ce3eada
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-06-03 23:32:34 +0900

    Delete apkbuild.sh

--- apkbuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd $1
-BUILDOZER=$(find / | grep bin | grep buildozer)
-echo $BUILDOZER
-buildozer init
-echo "android.accept_android_licenses = True" >> buildozer.spec
-buildozer -v android debug
