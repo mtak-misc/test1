9c0bafae468e0c3d2e9d651911f4dd45560c6b5f
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-06-03 23:28:08 +0900

    Update apkbuild.sh

--- apkbuild.sh
+++ apkbuild.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 cd $1
+BUILDOZER=$(find / | grep bin | grep buildozer)
+echo $BUILDOZER
 buildozer init
 echo "android.accept_android_licenses = True" >> buildozer.spec
 buildozer -v android debug
