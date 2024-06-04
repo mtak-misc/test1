15b33415b93d09b7a0d7b3881cf73b5edfd7390a
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-06-05 00:32:34 +0900

    Update main.py

--- sample/main.py
+++ sample/main.py
@@ -31,4 +31,8 @@ class ServiceApp(App):
 # https://github.com/kivy/python-for-android/issues/1908
 
 if __name__ == '__main__':
+    from kivy.utils import platform
+    if platform == "android":
+        from android.permissions import request_permissions, Permission
+        request_permissions([Permission.INTERNET])
     ServiceApp().run()
