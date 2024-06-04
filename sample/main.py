101292cdc7f963923817a40ef04bb89b3f33acb7
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-06-04 23:43:25 +0900

    Update main.py

--- sample/main.py
+++ sample/main.py
@@ -15,7 +15,7 @@ def create_webview(*args):
         wvc = WebViewClient();
         webview.setWebViewClient(wvc);
         activity.setContentView(webview)
-        webview.loadUrl('http://127.0.0.1/')
+        webview.loadUrl('http://www.google.com/')
 
 class Wv(Widget):
         def __init__(self, **kwargs):
