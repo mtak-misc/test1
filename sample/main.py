fe815d70d7506469ea399921aa8232497f0c5409
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-06-09 08:04:14 +0900

    Update main.py

--- sample/main.py
+++ sample/main.py
@@ -1,38 +1,8 @@
 from kivy.app import App
-from jnius import autoclass
-from kivy.clock import Clock
-from android.runnable import run_on_ui_thread
-from kivy.uix.widget import Widget
+from kivy.uix.button import Button
 
-WebView = autoclass('android.webkit.WebView')
-WebViewClient = autoclass('android.webkit.WebViewClient')
-activity = autoclass('org.kivy.android.PythonActivity').mActivity
+class TestApp(App):
+    def build(self):
+        return Button(text='Hello World')
 
-@run_on_ui_thread
-def create_webview(*args):
-        webview = WebView(activity)
-        webview.getSettings().setJavaScriptEnabled(True)
-        wvc = WebViewClient();
-        webview.setWebViewClient(wvc);
-        activity.setContentView(webview)
-        webview.loadUrl('http://httpbin.org/delay/3')
-
-class Wv(Widget):
-        def __init__(self, **kwargs):
-                super().__init__(**kwargs)
-                self.__functionstable__ = {}
-                Clock.schedule_once(create_webview, 0)
-
-
-class ServiceApp(App):
-        def build(self):
-                return Wv()
-
-# https://github.com/kivy/python-for-android/issues/1908
-
-if __name__ == '__main__':
-#    from kivy.utils import platform
-#    if platform == "android":
-#        from android.permissions import request_permissions, Permission
-#        request_permissions([Permission.INTERNET])
-    ServiceApp().run()
+TestApp().run()
