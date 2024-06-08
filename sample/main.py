b6cd3758e096b138e2da46f91edd608e0fd3b014
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-06-09 07:20:05 +0900

    Update main.py

--- sample/main.py
+++ sample/main.py
@@ -10,22 +10,22 @@ activity = autoclass('org.kivy.android.PythonActivity').mActivity
 
 @run_on_ui_thread
 def create_webview(*args):
-        webview = WebView(activity)
-        webview.getSettings().setJavaScriptEnabled(True)
-        wvc = WebViewClient();
-        webview.setWebViewClient(wvc);
-        activity.setContentView(webview)
-        webview.loadUrl('http://httpbin.org/delay/3')
+    webview = WebView(activity)
+    webview.getSettings().setJavaScriptEnabled(True)
+    wvc = WebViewClient();
+    webview.setWebViewClient(wvc);
+    activity.setContentView(webview)
+    webview.loadUrl('http://httpbin.org/delay/3')
 
 class Wv(Widget):
-        def __init__(self, **kwargs):
-                super().__init__(**kwargs)
-                self.__functionstable__ = {}
-                Clock.schedule_once(create_webview, 0)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.__functionstable__ = {}
+        Clock.schedule_once(create_webview, 0)
 
 class ServiceApp(App):
-        def build(self):
-                return Wv()
+    def build(self):
+        return Wv()
 
 # https://github.com/kivy/python-for-android/issues/1908
 
