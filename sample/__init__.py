7a23bb0d4e6d56c3e919db2a10bac1dd8e566d04
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-06-09 20:07:16 +0900

    Create __init__.py

---
+++ sample/__init__.py
@@ -0,0 +1,12 @@
+from pythonforandroid.recipe import RustCompiledComponentsRecipe
+
+
+class OrjsonRecipe(RustCompiledComponentsRecipe):
+    version = "3.10.3"
+    url = "https://github.com/ijl/orjson/archive/refs/tags/{version}.tar.gz"
+    use_maturin = True
+    hostpython_prerequisites = ["typing_extensions"]
+    site_packages_name = "orjson"
+
+
+recipe = PydanticcoreRecipe()
