fdde3a2822a4a6fe8192c90af0c35421aa72a51a
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-06-09 09:28:53 +0900

    Update main.py

--- sample/main.py
+++ sample/main.py
@@ -3,22 +3,10 @@ from jnius import autoclass
 from kivy.clock import Clock
 from android.runnable import run_on_ui_thread
 from kivy.uix.widget import Widget
-import gradio as gr
-from fastapi import FastAPI
-import uvicorn
-from threading import Thread
 
 WebView = autoclass('android.webkit.WebView')
 WebViewClient = autoclass('android.webkit.WebViewClient')
 activity = autoclass('org.kivy.android.PythonActivity').mActivity
-
-async def generate_text(message, history):
-    temp = ""
-    temp += 'Echo: ' + message
-    yield temp 
-
-def gradio_worker(app):
-    uvicorn.run(app, host="127.0.0.1", port=8080, log_level="info")
 
 @run_on_ui_thread
 def create_webview(*args):
@@ -32,23 +20,7 @@ def create_webview(*args):
 class Wv(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        demo = gr.ChatInterface(
-            fn=generate_text,
-            title="Gradio Sample",
-            cache_examples=False,
-            retry_btn=None,
-            undo_btn="Remove last",
-            clear_btn="Clear all",
-        )
-
-        app = FastAPI()
-        app = gr.mount_gradio_app(app, demo, path='/')
-
-        thread = Thread(target=gradio_worker, args=(app,))
-        thread.daemon = True
-        thread.start()
-        
-        self.__functionstable__ = {}
+                self.__functionstable__ = {}
         Clock.schedule_once(create_webview, 0)
 
 class ServiceApp(App):
