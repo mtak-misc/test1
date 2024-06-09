dac694e4a3832a3b4832f6b41c074e617f222061
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2024-06-09 09:12:35 +0900

    Update main.py

--- sample/main.py
+++ sample/main.py
@@ -37,7 +37,6 @@ async def generate_text(message, history):
 def gradio_worker(app):
     uvicorn.run(app, host="127.0.0.1", port=8080, log_level="info")
 
-
 if __name__ == '__main__':
     import gradio as gr
     demo = gr.ChatInterface(
